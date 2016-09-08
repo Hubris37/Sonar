e3c05ce0302c8fb7e86e6098fb45e8a2d7217d66
gylleus <gylleus@kth.se>
2016-09-08 12:51:32 +0200

    Cool cirkel

---
+++ Assets/Scripts/ShaderController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderController : MonoBehaviour {
+
+    public static Shader s;
+    public Renderer r;
+
+    public GameObject AudioSource;
+    private AudioMeasure audioMeasure;
+
+	// Use this for initialization
+	void Start () {
+        r = GetComponent<Renderer>();
+        r.material.shader = Shader.Find("Custom/Echolocation");
+        audioMeasure = AudioSource.GetComponent<AudioMeasure>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float val = 0.05f * (160 + audioMeasure.DbValue);
+        r.material.SetFloat("_Radius", val);
+	}
+}
