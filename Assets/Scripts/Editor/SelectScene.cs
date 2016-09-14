918421f1eadaeef5c26c69db067c31f9547626fb
Staffan <staffan.sandberg94@gmail.com>
2016-09-14 20:11:34 +0200

    Added handy Menu Item for changing scene

---
+++ Assets/Scripts/Editor/SelectScene.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.Collections;
+
+public class SelectScene : MonoBehaviour
+{
+    [MenuItem("Open Scene/Movement Test")]
+    public static void OpenWalkTest()
+    {
+        OpenScene("movementTest");
+    }
+
+    [MenuItem("Open Scene/Pitch Color Light")]
+    public static void OpenPitchColor()
+    {
+        OpenScene("dBPitchColorLight");
+    }
+
+    [MenuItem("Open Scene/Record And Playback")]
+    public static void OpenRecordAndPlayback()
+    {
+        OpenScene("recordAndPlayback");
+    }
+    static void OpenScene(string name)
+    {
+        if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            EditorSceneManager.OpenScene("Assets/Scenes/" + name + ".unity");
+    }	
+}
+
