91bbf333c599b30d41bb925d09fb4bc62f5855be
Palmberg <robinpa@kth.se>
2016-09-14 11:42:10 +0200

    Revert "Functional Maze Generation"
    This reverts commit 14dbc3ffff7c4374117ce7ceb5577a9712a668c9.

--- Assets/Scripts/MazeGeneratorScripts/MazeDirection.cs
+++ Assets/Scripts/MazeGeneratorScripts/MazeDirection.cs
@@ -11,6 +11,12 @@ public static class MazeDirections {
 
 	public const int Count = 4;
 
+	public static MazeDirection RandomValue {
+		get { 
+			return (MazeDirection)Random.Range (0, Count);
+		}
+	}
+
 	private static IntVector2[] vectors = {
 		new IntVector2(0, 1),
 		new IntVector2(1, 0),
@@ -18,39 +24,8 @@ public static class MazeDirections {
 		new IntVector2(-1, 0)
 	};
 
-    private static MazeDirection[] opposites = {
-        MazeDirection.South,
-        MazeDirection.West,
-        MazeDirection.North,
-        MazeDirection.East
-    };
-
-    private static Quaternion[] rotations = {
-        Quaternion.identity,
-        Quaternion.Euler(0f, 90f, 0f),
-        Quaternion.Euler(0f, 180f, 0f),
-        Quaternion.Euler(0f, 270f, 0f)
-    };
-
-    public static Quaternion ToRotation(this MazeDirection direction)
-    {
-        return rotations[(int)direction];
-    }
-
-    public static MazeDirection RandomValue
-    {
-        get
-        {
-            return (MazeDirection)Random.Range(0, Count);
-        }
-    }
-
-    public static IntVector2 ToIntVector2 (this MazeDirection direction) {
+	public static IntVector2 ToIntVector2 (this MazeDirection direction) {
 		return vectors[(int)direction];
 	}
-
-    public static MazeDirection GetOpposite(this MazeDirection direction) {
-        return opposites[(int)direction];
-    }
 
 }
