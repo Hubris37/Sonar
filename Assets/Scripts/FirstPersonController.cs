d8491a8a930493a0df0aa1a8888a9887ab56d4fe
Ahlisen <ahlisen93@gmail.com>
2016-09-20 20:43:08 +0200

    Added Goal and new movement

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class FirstPersonController : MonoBehaviour {
 
+	public event Action OnGoalTouch;
+
 	public float mouseSensitivityX = 3.5f;
 	public float mouseSensitivityY = 3.5f;
-	public float walkSpeed = 1;
+	public float walkSpeed = 1f;
+	public float turnSpeed = 2f;
 
 	Transform cameraT;
 	Rigidbody myRigidBody;
 	float verticalLookRotation;
+	public GameObject goal;
 
 	Vector3 moveAmount;
 	Vector3 smoothMoveVelocity;
@@ -23,18 +28,41 @@ public class FirstPersonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
 		transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
 		verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
 		verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60,60);
+		transform.Rotate(Vector3.right * Input.GetAxis("Mouse Y")* mouseSensitivityY);
 		cameraT.localEulerAngles = Vector3.left * verticalLookRotation;
 
 		Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 		Vector3 targetMoveAmount = moveDir * walkSpeed;
 		moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+		*/
 	}
 
 	void FixedUpdate() {
-		myRigidBody.MovePosition(myRigidBody.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+		//myRigidBody.MovePosition(myRigidBody.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+
+		float h = Input.GetAxis ("Horizontal") * Time.fixedDeltaTime * turnSpeed;
+		float v = Input.GetAxis ("Vertical") * Time.fixedDeltaTime * turnSpeed;
+		transform.Rotate (h * Vector3.back);
+		transform.Rotate (v * Vector3.right);
+		//myRigidBody.AddRelativeTorque (h * Vector3.back );
+		//myRigidBody.AddRelativeTorque (v * Vector3.right);
+
+		if (Input.GetButton ("Jump")) {
+			myRigidBody.AddRelativeForce (Vector3.forward * walkSpeed, ForceMode.Impulse);
+		}
+
+
+	}
+
+	void OnTriggerEnter(Collider triggerCollider) {
+		print (triggerCollider.gameObject.name);
+		if (triggerCollider.gameObject == goal) {
+			OnGoalTouch ();
+		}
 	}
 		
 }
--- Assets/Scripts/MazeGeneratorScripts/GameManager.cs
+++ Assets/Scripts/MazeGeneratorScripts/GameManager.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject Player;
+	FirstPersonController player;
+	GameObject goal;
+	public GameObject goalPrefab;
 	public Maze mazePrefab;
 	private Maze mazeInstance;
 
+	public int level = 1;
+
 	private void Start () {
+		goal = Instantiate (goalPrefab);
+		player = FindObjectOfType<FirstPersonController> ();
+		player.OnGoalTouch += WonGame;
+		player.goal = goal;
 		BeginGame();
 	}
 
 	private void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
+		if (Input.GetKeyDown(KeyCode.LeftControl)) {
 			RestartGame();
 		}
 	}
@@ -20,12 +28,23 @@ public class GameManager : MonoBehaviour {
 	private void BeginGame () {
 		mazeInstance = Instantiate (mazePrefab) as Maze;
 		mazeInstance.Generate();
+
 		Vector3 pos = mazeInstance.GetCell (new IntVector2 (0, 0)).transform.position;
-		Player.transform.position = new Vector3(pos.x, pos.y+.1f, pos.z);
+		player.transform.position = new Vector3(pos.x, pos.y+.1f, pos.z);
+
+		pos = mazeInstance.GetCell (new IntVector2 (mazeInstance.size.x - 1, mazeInstance.size.z - 1)).transform.position;
+		goal.transform.position = new Vector3(pos.x, pos.y+0.5f, pos.z);
 	}
 
 	private void RestartGame () {
 		Destroy (mazeInstance.gameObject);
+		level = 1;
+		BeginGame ();
+	}
+
+	private void WonGame() {
+		Destroy (mazeInstance.gameObject);
+		level++;
 		BeginGame ();
 	}
 }
