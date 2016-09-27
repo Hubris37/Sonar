c88c374c19991fba4156d644611287196f36b444
Staffan <staffan.sandberg94@gmail.com>
2016-09-27 22:46:56 +0200

    Added experimental arrow above player in observer view.

---
+++ Assets/Scripts/UI/ArrowFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowFaceCamera : MonoBehaviour 
+{
+	public Camera targetCamera;
+
+	void Start()
+	{
+		targetCamera = GameObject.Find("OverviewCamera").GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// get vector to cam
+		Vector3 objToCam = targetCamera.transform.position - transform.position;
+		// only use the up vector y
+     	objToCam.x = objToCam.z = 0.0f;
+		// look at camera only around the y axis
+      	transform.LookAt( targetCamera.transform.position - objToCam); 
+	}
+}
