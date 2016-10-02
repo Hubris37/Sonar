5de073f0742583837fc354b05efbfc43d47e1fa9
apan1000 <fredrik.911225@gmail.com>
2016-10-03 00:56:17 +0200

    Wiimote plugin + test scene

---
+++ Assets/Scripts/WiiMoteController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using WiimoteApi;
+
+public class WiiMoteController : MonoBehaviour {
+	 bool[] leds = new bool[] {true, false, false, false};
+	 int count = 0;
+	 float prevTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		InitWiimotes();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+			int ret;
+			do {
+				ret = remote.ReadWiimoteData();
+			} while (ret > 0); // ReadWiimoteData() returns 0 when nothing is left to read.  So by doing this we continue to
+									// update the Wiimote until it is "up to date."
+			float[] pointer = remote.Ir.GetPointingPosition();
+			if(pointer[0] > 0 && pointer[1] > 0) {
+				transform.position = new Vector3(
+					Mathf.Clamp(transform.position.x + (pointer[0]-.5f), -5, 5),
+					transform.position.y,
+					Mathf.Clamp(transform.position.z + (pointer[1]-.5f), -5, 5)
+					);
+			}
+		}
+
+		if(Time.time - prevTime > .5f) {
+			prevTime = Time.time;
+			count += 1;
+			foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+				int a = (count+1)%2;
+				int b = (count+0)%2;
+				int c = (count+1)%2;
+				int d = (count+0)%2;
+				remote.SendPlayerLED(leds[a], leds[b], leds[c], leds[d]);
+			}
+		} else {
+
+		}
+	}
+
+	void OnApplicationQuit() {
+		FinishedWithWiimotes();
+	}
+
+	void InitWiimotes() {
+		WiimoteManager.FindWiimotes(); // Poll native bluetooth drivers to find Wiimotes
+		foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+			remote.SendPlayerLED(true, false, false, false);
+			remote.SetupIRCamera(IRDataType.BASIC); // Basic IR dot position data
+		}
+	}
+	void FinishedWithWiimotes() {
+		foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+			WiimoteManager.Cleanup(remote);
+		}
+	}
+}
