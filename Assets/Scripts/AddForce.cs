7816e5a347abfb98e0b6959e9e517cd395997e36
Ahlisen <ahlisen93@gmail.com>
2016-10-05 16:34:18 +0200

    fixed door kinda

--- Assets/Scripts/AddForce.cs
+++ Assets/Scripts/AddForce.cs
@@ -5,12 +5,24 @@ public class AddForce : MonoBehaviour {
 
 	Rigidbody rigidbody;
 	FireSoundWave blaster;
+	public Vector3 offset;
+	private Vector3 relativeOffset;
+
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody>();
 		//blaster = FindObjectOfType<FireSoundWave> ();
 		FireSoundWave.onBlastHit += RecieveForce;
+		relativeOffset = transform.TransformPoint(offset);//transform.InverseTransformPoint(transform.position + offset);
+
 	}
+	/*
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+		relativeOffset = transform.TransformPoint(offset);
+        Gizmos.DrawSphere(relativeOffset, 0.1f);
+    }*/
+
 	
 	// Update is called once per frame
 	void FixedUpdate () {
@@ -18,8 +30,8 @@ public class AddForce : MonoBehaviour {
 	}
 
 	void RecieveForce(Vector3 hitPos, float pitchVal, float dbVal) {
-
-		Vector3 heading = (transform.position - hitPos)*0.2f;
+		Vector3 myPos = relativeOffset;// + relativeOffset;// + (transform.localPosition + offset);
+		Vector3 heading = (myPos - hitPos);
 		float dist = heading.sqrMagnitude;
 		Vector3 dir = heading / dist;
 		//float dist = Vector3.Distance(hitPos, transform.position);
