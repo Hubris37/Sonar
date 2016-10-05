c2270df176a264e5b872dab675e32af1ae776a40
apan1000 <fredrik.911225@gmail.com>
2016-10-05 16:55:02 +0200

    Mixer settings

--- Assets/Scripts/WiiMoteController.cs
+++ Assets/Scripts/WiiMoteController.cs
@@ -5,12 +5,17 @@ using WiimoteApi;
 public class WiiMoteController : MonoBehaviour {
 	 bool[] leds = new bool[] {true, false, false, false};
 	 int count = 0;
+	 bool blink = false;
 	 float prevTime = 0;
-	 Camera cam;
+	 public Camera cam;
+
+	 private Vector3 screenPoint;
 
 	// Use this for initialization
 	void Start () {
-		cam = Camera.main;
+		if(cam == null) {
+			cam = Camera.main;
+		}
 		InitWiimotes();
 	}
 	
@@ -22,11 +27,24 @@ public class WiiMoteController : MonoBehaviour {
 				ret = remote.ReadWiimoteData();
 			} while (ret > 0); // ReadWiimoteData() returns 0 when nothing is left to read.  So by doing this we continue to
 									// update the Wiimote until it is "up to date."
+
+			screenPoint = cam.WorldToScreenPoint(transform.position);
+
 			float[] pointer = remote.Ir.GetPointingPosition();
 			if(pointer[0] > 0 && pointer[1] > 0) {
-				float distance = transform.position.y - cam.transform.position.y;
-				Vector3 pos = new Vector3(pointer[0]*Screen.width, distance, pointer[1]*Screen.height);
-				transform.position = cam.ScreenToWorldPoint(pos);
+				Vector3 curScreenPoint = new Vector3(pointer[0]*Screen.width, pointer[1]*Screen.height, screenPoint.z);
+				Vector3 curPosition = cam.ScreenToWorldPoint(curScreenPoint);
+				transform.position = curPosition;
+
+
+
+				// float distance = transform.position.y - cam.transform.position.y;
+				// Vector3 pos = new Vector3(pointer[0]*Screen.width, distance, pointer[1]*Screen.height);
+				// transform.position = cam.ScreenToWorldPoint(pos);
+
+
+
+
 				// transform.position = new Vector3(
 				// 	Mathf.Clamp(transform.position.x + (pointer[0]-.5f), -5, 5),
 				// 	transform.position.y,
@@ -35,18 +53,20 @@ public class WiiMoteController : MonoBehaviour {
 			}
 		}
 
-		if(Time.time - prevTime > .5f) {
-			prevTime = Time.time;
-			count += 1;
-			foreach(Wiimote remote in WiimoteManager.Wiimotes) {
-				int a = (count+1)%2;
-				int b = (count+0)%2;
-				int c = (count+1)%2;
-				int d = (count+0)%2;
-				remote.SendPlayerLED(leds[a], leds[b], leds[c], leds[d]);
-			}
-		} else {
+		if(blink) {
+			if(Time.time - prevTime > .5f) {
+				prevTime = Time.time;
+				count += 1;
+				foreach(Wiimote remote in WiimoteManager.Wiimotes) {
+					int a = (count+1)%2;
+					int b = (count+0)%2;
+					int c = (count+1)%2;
+					int d = (count+0)%2;
+					remote.SendPlayerLED(leds[a], leds[b], leds[c], leds[d]);
+				}
+			} else {
 
+			}
 		}
 	}
 
