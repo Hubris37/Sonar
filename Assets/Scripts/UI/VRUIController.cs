6192e006786dd9f68ae5bdcceb6c1a0eb5fa9d48
Staffan <staffan.sandberg94@gmail.com>
2016-10-06 19:14:34 +0200

    GameOver screen for VR player

---
+++ Assets/Scripts/UI/VRUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Decides when different UIs for the VR user should be shown.
+public class VRUIController : MonoBehaviour 
+{
+
+	public GameObject gameOverUI;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameManager.isDead += ShowGameOver;
+		GameManager.isReborn += HideGameOver;
+	}
+	
+	private void ShowGameOver()
+	{
+		gameOverUI.SetActive(true);
+	}
+
+	private void HideGameOver()
+	{
+		gameOverUI.SetActive(false);
+	}
+}
