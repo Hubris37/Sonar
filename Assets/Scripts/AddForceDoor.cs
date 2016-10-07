130e59c0c8e51a9efcdde3bc7308bd960d425df3
Ahlisen <ahlisen93@gmail.com>
2016-10-07 11:55:37 +0200

    Fixed Speaker prefab and more

--- Assets/Scripts/AddForceDoor.cs
+++ Assets/Scripts/AddForceDoor.cs
@@ -1,23 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AddForce : MonoBehaviour {
+public class AddForceDoor : MonoBehaviour {
 
 	Rigidbody myRigidbody;
 	FireSoundWave blaster;
 	public Vector3 offset;
 	private Vector3 relativeOffset;
 
+	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody>();
 		FireSoundWave.onBlastHit += RecieveForce;
 		relativeOffset = transform.TransformPoint(offset);
 	}
 
+	/*
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+		relativeOffset = transform.TransformPoint(offset);
+        Gizmos.DrawSphere(relativeOffset, 0.1f);
+    }*/
+
 	void RecieveForce(Vector3 hitPos, float pitchVal, float dbVal) {
 		Vector3 myPos = transform.TransformPoint(offset);
 		Vector3 heading = (myPos - hitPos);
-		float dist = heading.sqrMagnitude;
+		Vector2 xz = new Vector2(heading.x,heading.z);
+		float dist = xz.sqrMagnitude;
 		Vector3 dir = heading / dist;
 
 		myRigidbody.AddForce(dir*(dbVal*250/(dist+20f)), ForceMode.Impulse);
