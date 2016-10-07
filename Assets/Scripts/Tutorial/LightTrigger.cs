764695b19c39e5ed7302826bd51252cf13f74490
Staffan Sandberg <Staffan.Sandberg94@gmail.com>
2016-10-07 15:03:03 +0200

    Tutorial arent V0.2

---
+++ Assets/Scripts/Tutorial/LightTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class LightTrigger : MonoBehaviour 
+{
+	public event Action LightZoneEnter;
+	public event Action DarkZoneEnter;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+			DarkZoneEnter();		
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+			LightZoneEnter();
+	}
+}
--- Assets/Scripts/Tutorial/TutorialController.cs
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -8,9 +8,32 @@ public class TutorialController : MonoBehaviour
 	public TutorialGoal tutGoal;
 	public string nextLvlName;
 
+	[Header("Lights")] 	
+	public Light dirLight;
+	public float lowestIntensity = 0f;
+	public float highestIntensity = 1f;
+	public LightTrigger lightTrigger;
+
 	void Start()
 	{
 		tutGoal.OnGoalEnter += ChangeScene;
+		lightTrigger.DarkZoneEnter += DarkZone;
+		lightTrigger.LightZoneEnter += LightZone;
+	}
+
+
+	void LightZone()
+	{
+/*		StopCoroutine(DimLight(0));
+		StartCoroutine(DimLight(highestIntensity));*/
+		dirLight.intensity = highestIntensity;
+	}
+
+	void DarkZone()
+	{
+		/*StopCoroutine(DimLight(0));
+		StartCoroutine(DimLight(lowestIntensity));*/
+		dirLight.intensity = lowestIntensity;
 	}
 
 	private void ChangeScene()
@@ -20,6 +43,16 @@ public class TutorialController : MonoBehaviour
 			NiceSceneTransition.instance.LoadScene(nextLvlName);
 		} else {
 			SceneManager.LoadScene(nextLvlName);
+		}
+	}
+
+	IEnumerator DimLight(float dimTo)
+	{
+		while(dirLight.intensity != dimTo)
+		{
+			float curIntens = dirLight.intensity;
+			dirLight.intensity = Mathf.Lerp(curIntens, dimTo, Time.deltaTime);
+			yield return null;
 		}
 	}
 }
