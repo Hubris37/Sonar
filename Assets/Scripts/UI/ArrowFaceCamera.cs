7e38cdc49d898ed89cf699334f3c0f5eaa2d6bc7
Staffan <staffan.sandberg94@gmail.com>
2016-10-31 18:38:53 +0100

    Player arrow anim and rot fixed.

--- Assets/Scripts/UI/ArrowFaceCamera.cs
+++ Assets/Scripts/UI/ArrowFaceCamera.cs
@@ -3,21 +3,21 @@ using System.Collections;
 
 public class ArrowFaceCamera : MonoBehaviour 
 {
-	public Camera targetCamera;
+	private GameObject arrowTarget;
 
 	void Start()
 	{
-		targetCamera = GameObject.Find("OverviewCamera").GetComponent<Camera>();
+		arrowTarget = GameObject.Find("ArrowTarget");
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		// get vector to cam
-		Vector3 objToCam = targetCamera.transform.position - transform.position;
-		// only use the up vector y
-     	objToCam.x = objToCam.z = 0.0f;
-		// look at camera only around the y axis
-      	transform.LookAt( targetCamera.transform.position - objToCam); 
+		transform.LookAt(arrowTarget.transform);
+		
+		/*Vector3 currentRot = transform.localEulerAngles;
+		currentRot.y = currentRot.z = 0;
+		currentRot = -currentRot;
+		transform.Rotate(currentRot,Space.World);*/
 	}
 }
